4c74fb65a9fd35f3d81d27c7c7d8f6308a26216d
GabrielF-C <45073321+GabrielF-C@users.noreply.github.com>
2024-10-07 00:58:06 -0400

    Update version number

--- script.user.js
+++ script.user.js
@@ -5,7 +5,7 @@
 // @icon         https://img.icons8.com/?size=100&id=47442&format=png&color=40C057
 // @namespace    https://github.com/GabrielF-C/comparison-price-converter
 
-// @version      20241006_221128
+// @version      20241007_005756
 // @downloadURL  https://github.com/GabrielF-C/comparison-price-converter/raw/main/script.user.js
 // @updateURL    https://github.com/GabrielF-C/comparison-price-converter/raw/main/script.user.js
 
